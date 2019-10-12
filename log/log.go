3b0251f2045c4abdd4d24e34b2cded5a95cf27be
Xiaobo Zhu <xiaobo.zhu@shopee.com>
2019-10-12 09:53:54 +0800

    Refactor log

--- log/log.go
+++ log/log.go
@@ -21,15 +21,6 @@ import (
 //	serverLog.InfoF("start failed")
 // ```
 
-var verbose = flag.Bool("verbose", true, "indicate whether print log on console")
-var logLevelMaps = map[int]string{
-	INFO:  "INFO",
-	DEBUG: "DEBUG",
-	WARN:  "WARN",
-	ERROR: "ERROR",
-	FATAL: "FATAL",
-}
-
 const (
 	INFO = iota
 	DEBUG
@@ -38,122 +29,112 @@ const (
 	FATAL
 )
 
-var globalLogLock sync.RWMutex
+var (
+	verbose      = flag.Bool("verbose", true, "indicate whether print log on console")
+	logLevelMaps = map[int]string{
+		INFO:  "INFO",
+		DEBUG: "DEBUG",
+		WARN:  "WARN",
+		ERROR: "ERROR",
+		FATAL: "FATAL",
+	}
+	fileLog            *SimpleLog
+	globalLogLock      sync.RWMutex
+	globalLogger       = map[string]SimpleLogWrapper{}
+	ErrReInitializeLog = errors.New("log have been initialized.")
+	ErrClosedLog       = errors.New("log have been closed")
+	logBufChCapacity   = 1 << 16
+)
 
 type SimpleLog struct {
 	SavePath   string
 	BufferSize int
 	Buf        *bytes.Buffer
-	BufLock    sync.Mutex
+	lock       sync.Mutex
 	logFlusher *logFlusher
 	logCh      chan *bytes.Buffer
 	console    bool
+	closed     bool
 }
 
 type SimpleLogWrapper struct {
-	log    *SimpleLog
 	header string
 }
 
-var globalLogger = map[string]*SimpleLog{}
-
-func GetLog(logName string) *SimpleLog {
+func GetLog(logName string) SimpleLogWrapper {
 	globalLogLock.RLock()
 	defer globalLogLock.RUnlock()
+	_, ok := globalLogger[logName]
+	if !ok {
+		globalLogger[logName] = SimpleLogWrapper{logName}
+	}
 	return globalLogger[logName]
 }
 
-func CloseLog(logName string) {
-	log, ok := globalLogger[logName]
-	if !ok {
-		return
-	}
-	log.closeLogger()
+func CloseLog() error {
+	return fileLog.closeLogger()
 }
 
-var ErrReInitializeLog = errors.New("log have been initialized.")
-
-var logBufChCapacity = 1 << 16
-
-// Add a file logger that would print logs to files and also print logs to console if option `verbose` is opened.
-func InitFileLogger(logName, savePath string, bufSize int) error {
+func InitLogger(savePath string, bufSize int) error {
 	globalLogLock.Lock()
 	defer globalLogLock.Unlock()
-	_, ok := globalLogger[logName]
-	if ok {
-		return ErrReInitializeLog
+	if fileLog == nil {
+		logCh := make(chan *bytes.Buffer, logBufChCapacity)
+		flusher, err := newLogFlusher(savePath, logCh)
+		if err != nil {
+			return err
+		}
+		fileLog = &SimpleLog{
+			SavePath:   savePath,
+			BufferSize: bufSize,
+			Buf:        new(bytes.Buffer),
+			lock:       sync.Mutex{},
+			logFlusher: flusher,
+			logCh:      logCh,
+			console:    *verbose,
+		}
+		go flusher.flushLog()
 	}
-	logCh := make(chan *bytes.Buffer, logBufChCapacity)
-	flusher, err := newLogFlusher(savePath, logCh)
-	if err != nil {
-		return err
-	}
-	newLogger := &SimpleLog{
-		SavePath:   savePath,
-		BufferSize: bufSize,
-		Buf:        new(bytes.Buffer),
-		BufLock:    sync.Mutex{},
-		logFlusher: flusher,
-		logCh:      logCh,
-	}
-	globalLogger[logName] = newLogger
-	go flusher.flushLog()
-	return nil
-}
-
-// Add a console logger that would only print logs to console.
-func InitConsoleLogger(logName string) error {
-	globalLogLock.Lock()
-	defer globalLogLock.Unlock()
-	_, ok := globalLogger[logName]
-	if ok {
-		return ErrReInitializeLog
-	}
-	newLogger := &SimpleLog{
-		console: true,
-	}
-	globalLogger[logName] = newLogger
 	return nil
 }
 
 func (log SimpleLogWrapper) InfoF(format string, params ...interface{}) {
-	log.log.printLog(log.header, INFO, format, params...)
+	fileLog.printLog(log.header, INFO, format, params...)
 }
 
 func (log SimpleLogWrapper) DebugF(format string, params ...interface{}) {
-	log.log.printLog(log.header, DEBUG, format, params...)
+	fileLog.printLog(log.header, DEBUG, format, params...)
 }
 
 func (log SimpleLogWrapper) WarnF(format string, params ...interface{}) {
-	log.log.printLog(log.header, WARN, format, params...)
+	fileLog.printLog(log.header, WARN, format, params...)
 }
 
 func (log SimpleLogWrapper) ErrorF(format string, params ...interface{}) {
-	log.log.printLog(log.header, ERROR, format, params...)
+	fileLog.printLog(log.header, ERROR, format, params...)
 }
 
 func (log SimpleLogWrapper) FatalF(format string, params ...interface{}) {
-	log.log.printLog(log.header, FATAL, format, params...)
+	fileLog.printLog(log.header, FATAL, format, params...)
 }
 
-func (log SimpleLogWrapper) GetUnderlineLog() *SimpleLog {
-	return log.log
-}
-
-func (log *SimpleLog) AddHeader(header string) SimpleLogWrapper {
-	return SimpleLogWrapper{header: header, log: log}
-}
-
-func (log *SimpleLog) closeLogger() {
-	if log.logCh != nil {
+func (log *SimpleLog) closeLogger() error {
+	log.lock.Lock()
+	defer log.lock.Unlock()
+	if !log.closed {
 		log.doFlushIfNeed(true)
 		close(log.logCh)
+		log.closed = true
+		return nil
 	}
+	return ErrClosedLog
 }
 
 // PrintLog print a log with format like:
 // 2006/06/12 00:00:00.000000 [INFO] some thing happened.
 func (log *SimpleLog) printLog(header string, level int, format string, a ...interface{}) {
+	log.lock.Lock()
+	defer log.lock.Unlock()
 	l := fmt.Sprintf("%s [%s] [%s]: ", time.Now().Format("2006/06/12 00:00:00.000000"), header, logLevelMaps[level])
 	l = fmt.Sprintf(l+format, a...)
 	if log.console {
@@ -163,8 +144,6 @@ func (log *SimpleLog) printLog(header string, level int, format string, a ...int
 	if *verbose {
 		println(l)
 	}
-	log.BufLock.Lock()
-	defer log.BufLock.Unlock()
 	log.Buf.WriteString(l)
 	log.Buf.WriteByte('\n')
 	log.doFlushIfNeed(false)
